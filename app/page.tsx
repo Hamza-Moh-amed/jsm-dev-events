8fbdb30f59164be3cd4edd4d564790cd392741d8
Hamza-Moh-amed <hamzamohamedworld@gmail.com>
2026-08-17 16:43:42 +0300

    project setup, create header

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import ExploreBtn from "@/components/ExploreBtn";
+
+export default function Home() {
+  return (
+   <section>
+    <h1 className="text-center">The Hub for every dev <br /> Event you can't miss</h1>
+    <p className="text-center mt-5">Hackathons, Meetups, and conferences, all in one place</p>
+
+    <ExploreBtn />
+   </section>
+  );
+}
