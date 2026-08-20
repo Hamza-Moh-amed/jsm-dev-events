3dffb127ee318174e6bea1a8d45747513513f65a
Hamza-Moh-amed <hamzamohamedworld@gmail.com>
2026-08-20 11:45:58 +0300

    Refactor event details and booking components
    - Updated the event details page to display similar events.
    - Enhanced the booking component to accept event ID and slug for better functionality.
    - Improved event card linking for better navigation.
    - Adjusted CSS variables for consistency and readability.

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -26,6 +26,9 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({message: "Image file is Required"}, {status: 400})
         }
 
+        let tags = JSON.parse(formData.get("tags") as string)
+        let agenda = JSON.parse(formData.get("agenda") as string)
+
         const arrayBuffer = await file.arrayBuffer()
         const buffer = Buffer.from(arrayBuffer)
 
@@ -39,7 +42,7 @@ export async function POST(req: NextRequest) {
 
         event.image = (uploadResult as {secure_url: string}).secure_url
 
-        const createdEvent = await Event.create(event)
+        const createdEvent = await Event.create({...event, agenda: agenda, tags: tags})
 
         return NextResponse.json({message: "Event created Successfully", event: createdEvent}, {status: 201})
         
